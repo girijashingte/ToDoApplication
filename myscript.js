99edaeb1f6de120082ae107c95cf67ba2b18d869
girijashingte <46443785+girijashingte@users.noreply.github.com>
2019-07-31 10:52:27 +0100

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,44 @@
+
+// var p = document.querySelectorAll('p')
+
+// p.forEach(function(p){
+//     if(p.textContent.toLowerCase().includes('the')){
+//         p.remove()
+//     }
+// })
+
+// const todo = [
+//     {
+//         'text':'Learn Javascript',
+//         'completed': false
+//     },
+//     {
+//         'text':'Go to the gym',
+//         'completed': false
+//     },
+//     {
+//         'text':'Have breakfast',
+//         'completed': true
+//     },
+//     {
+//         'text':'brush',
+//         'completed': true
+//     },
+//     {
+//         'text':'Be calm',
+//         'completed': true
+//     },
+// ]
+
+// todo.forEach(function(item){
+//     if(!item.completed){
+//         const paragraph = document.createElement('p')
+//         paragraph.textContent = item.text
+//         document.querySelector('body').appendChild(paragraph)
+//     }
+// })
+
+// document.querySelector('Button').addEventListener("click",function(e){
+//     console.log('Button clicked')
+//     e.target.textContent = 'Button clicked'
+// })
